087fd3d182d1d90cc9ac7cf9c5a0fd2c8f55e7e9
Edwin Torres <codesandtags@gmail.com>
2017-12-16 23:32:25 -0500

    feat: Added solution for simple array sum in Javascript

---
+++ exercises/hackerrank/algorithms/warmup/simple-array-sum/README.md
@@ -0,0 +1,18 @@
+Given an array of *N* integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array. 
+The second line contains  space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+Sample Output
+
+31
---
+++ exercises/hackerrank/algorithms/warmup/simple-array-sum/javascript/solution.js
@@ -0,0 +1,36 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function simpleArraySum(n, ar) {
+    return ar.reduce((before, current) => {
+        return before + current;
+    }, 0);
+}
+
+function main() {
+    var n = parseInt(readLine());
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = simpleArraySum(n, ar);
+    process.stdout.write("" + result + "\n");
+
+}
