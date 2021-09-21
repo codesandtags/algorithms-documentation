6fa22165ec85cc6200a7ba5fc4f9fbc4102b2251
Edwin Torres <codesandtags@gmail.com>
2021-09-20 21:59:35 -0400

    feature: Added recursive function to add values to an array

---
+++ exercises/cracking-code-interview/bigO/recursion-array.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+// Only change code above this line
+
+countdown(40);
