2f6524b38b10acb50a61c8559fbb7aaaa40c3da2
Edwin Torres <codesandtags@gmail.com>
2022-02-24 22:56:59 -0500

    docs: Added ZTM exercises

---
+++ exercises/zero-to-mastery/big-o/exercise-1.js
@@ -0,0 +1,15 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+// Result: 3 + 4n = O (3 + 4n) = O(n)
