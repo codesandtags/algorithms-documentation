984b4df8bcc56f8c6c0ac3039ea0aeb6196a3e44
Edwin Torres <codesandtags@gmail.com>
2022-04-05 22:58:59 -0400

    feat: Added sort

---
+++ exercises/fundamentals/sorts/selection-sort.js
@@ -0,0 +1,17 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let lowestNumberIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowestNumberIndex]) {
+        lowestNumberIndex = j;
+      }
+    }
+
+    if (lowestNumberIndex != i) {
+      let temp = array[i];
+      array[i] = array[lowestNumberIndex];
+      array[lowestNumberIndex] = temp;
+    }
+  }
+  return array;
+}
