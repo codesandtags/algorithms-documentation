492e2a8ceee96ae3d30e823f184155cdebfb73e8
Edwin Torres <codesandtags@gmail.com>
2022-11-02 07:37:02 -0400

    chore: small changes on exercises

--- exercises/algoexperts/non-constructible-change.js
+++ exercises/algoexperts/non-constructible-change.js
@@ -14,3 +14,22 @@ function nonConstructibleChange(coins) {
 
 // Do not edit the line below.
 exports.nonConstructibleChange = nonConstructibleChange
+
+// Time:  O(n log n)
+// Space: O(1) sort in place
+function nonConstructibleChange(coins) {
+   coins.sort((a, b) => a - b) // ascendent
+   let currentChangeCreated = 0
+
+   for (let coin of coins) {
+      if (coin > currentChangeCreated + 1) {
+         break
+      }
+      currentChangeCreated += coin
+   }
+
+   return currentChangeCreated + 1
+}
+
+// Do not edit the line below.
+exports.nonConstructibleChange = nonConstructibleChange
