daf81c35aee16b8328b1113fa62d83f373630eaf
Edwin Torres <codesandtags@gmail.com>
2023-08-24 14:03:28 -0400

    feat: added javascript algorithms

--- exercises/common/recursion/reverse-string.js
+++ exercises/common/recursion/reverse-string.js
@@ -1,20 +1,51 @@
 function reverseStringRecursive(str) {
-  return str === ""
-    ? ""
-    : reverseStringRecursive(str.substr(1)) + str.charAt(0);
+   return str === ''
+      ? ''
+      : reverseStringRecursive(str.substr(1)) + str.charAt(0)
 }
 
-reverseStringRecursive("This is sparta"); // atraps si sihT
-
+reverseStringRecursive('This is sparta') // atraps si sihT
 
 function reverseString(message) {
-  // Base
-  if (message.length <= 1) {
-      return message;
-  }
+   // Base
+   if (message.length <= 1) {
+      return message
+   }
 
-  // Return recursion
-  return reverseString(message.substr(1)) + message.charAt(0);
+   // Return recursion
+   return reverseString(message.substr(1)) + message.charAt(0)
 }
 
-console.log(reverseString(message));
+console.log(reverseString(message))
+
+// Implementa una función que voltee el
+// contenido de un string, usando recursión
+// Es decir, no for, while, ni métodos
+// utilitarios como Array.reverse
+//
+// Ejemplo:
+// This is Sparta => atrapS si sihT
+
+// Implementa una función que voltee el
+// contenido de un string, usando recursión
+// Es decir, no for, while, ni métodos
+// utilitarios como Array.reverse
+//
+// Ejemplo:
+// This is Sparta => atrapS si sihT
+
+// Paso 0: Pensar en la solución
+// Paso 1: Definir función
+function reverse(text) {
+   // Paso 2: Definir caso base
+   if (text === '') {
+      return text
+   }
+   console.log(text)
+
+   // Paso 3: Aplicar recursión
+   return reverse(text.substr(1)) + text.at(0)
+}
+
+// Paso 4: Probar que funcione
+reverse('This is sparta')
