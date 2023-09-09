d727f1755cf41ce4f20aff4a2c38d2d688dd8b7c
Edwin Torres <codesandtags@gmail.com>
2023-09-09 17:12:51 -0400

    feat: added more solutions

---
+++ labs/frontend/javascript/get-elements-by-classname.js
@@ -0,0 +1,33 @@
+function isSubset(a, b) {
+   return Array.from(a).every((value) => b.contains(value))
+}
+
+/**
+ * @param {Element} element
+ * @param {string} classNames
+ * @return {Array<Element>}
+ */
+export default function getElementsByClassName(rootElement, classNames) {
+   const elements = []
+   const classNamesSet = new Set(classNames.trim().split(/\s+/))
+
+   function traverse(element) {
+      if (element == null) {
+         return
+      }
+
+      if (isSubset(classNamesSet, element.classList)) {
+         elements.push(element)
+      }
+
+      for (const child of element.children) {
+         traverse(child)
+      }
+   }
+
+   for (const child of rootElement.children) {
+      traverse(child)
+   }
+
+   return elements
+}
---
+++ labs/frontend/javascript/get-elements-by-tag-name.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Element} rootElement
+ * @param {string} tagName
+ * @return {Array<Element>}
+ */
+export default function getElementsByTagName(rootElement, tagNameParam) {
+   const elements = []
+   const tagName = tagNameParam.toUpperCase()
+
+   function traverse(element) {
+      if (element == null) {
+         return
+      }
+
+      if (element.tagName === tagName) {
+         elements.push(element)
+      }
+
+      for (const child of element.children) {
+         traverse(child)
+      }
+   }
+
+   for (const child of rootElement.children) {
+      traverse(child)
+   }
+
+   return elements
+}
