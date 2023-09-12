bde4be1d56020b420830348fa78ecc9d81e682f4
Edwin Torres <codesandtags@gmail.com>
2023-09-11 23:03:20 -0400

    feat: added solutions

---
+++ labs/frontend/javascript/drop-while.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} predicate - The function invoked per iteration.
+ * @return {Array} Returns the slice of `array`.
+ */
+export default function dropWhile(array, predicate) {
+   let index = 0
+
+   while (index < array.length && predicate(array[index], index, array)) {
+      index++
+   }
+
+   return array.slice(index)
+}
