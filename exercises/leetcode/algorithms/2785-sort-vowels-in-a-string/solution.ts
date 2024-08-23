ccbdb452cf0515c74e3fdb1281c6699022d40e46
Edwin Torres <codesandtags@gmail.com>
2024-08-23 00:14:07 -0400

    docs: added exercise 2785. leetcode

---
+++ exercises/leetcode/algorithms/2785-sort-vowels-in-a-string/solution.ts
@@ -0,0 +1,17 @@
+function sortVowels(s: string): string {
+   const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+   const sortedVowel = s.split('').filter((c) => vowels.has(c))
+   sortedVowel.sort()
+
+   let result = ''
+   let vowelIndex = 0
+   for (let i = 0; i < s.length; i++) {
+      if (vowels.has(s[i])) {
+         result += sortedVowel[vowelIndex++]
+      } else {
+         result += s[i]
+      }
+   }
+
+   return result
+}
